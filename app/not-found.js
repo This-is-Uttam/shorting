2268b59d661084edb602a35bf14624fd8d12f6f5
Your Name <uttam592k@gmail.com>
2025-12-01 16:31:50 +0530

    fix bugs

--- app/not-found.js
+++ app/not-found.js
@@ -8,7 +8,7 @@ const notFound = () => {
 
       <div className='m-auto w-fit my-[25vh]'>
         <div className='text-9xl text-black bg-yellow-500 text-center rounded-2xl p-4 font-bold'>404</div>
-        <h1 className='text-white text-5xl text-yellow-400 my-5 font-bold'>Page Not Found!</h1>
+        <h1 className='text-white text-5xl  my-5 font-bold'>Page Not Found!</h1>
         <p className='text-white text-xl text-center font-semibold'>Sorry, This URL does not exists.</p>
         <hr className='text-yellow-400 my-5' />
 
